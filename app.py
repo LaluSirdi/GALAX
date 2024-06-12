c99a5bbeebc0bb81d46962f7195560e8624825ae
LaluSirdi <laloesirdy@google.com>
2024-06-12 21:53:06 +0800

    update upmerch

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-import os
-from flask import Flask, render_template, request, redirect, url_for
-from werkzeug.utils import secure_filename
+from flask import Flask, redirect, url_for, render_template, request, jsonify
 from pymongo import MongoClient
-from bson.objectid import ObjectId
-app = Flask(__name__)
+from bson import ObjectId
+import os
 
-client = MongoClient('mongodb+srv://laloesirdy:mama1000@cluster0.t2dmoad.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-db = client['coba'] 
-collection = db.coba  
+app = Flask(__name__)
+client = MongoClient('mongodb+srv://nawangandrian:xfDGGaRjSPR5TPoJ@cluster0.eqhmd7k.mongodb.net/')
+db = client.dbfjkt
 
 @app.route('/')
 def index():
@@ -19,65 +17,90 @@ def beranda():
 
 @app.route('/merchandise')
 def merchandise():
-    return render_template('merchandise.html')
+    merchandise = list(db.merchandise.find({}))
+    return render_template('merchandise.html', merchandise=merchandise)
+
+@app.route('/beli')
+def beli():
+    nama = request.args.get('nama')
+    harga = request.args.get('harga')
+    whatsapp = '6285860354633'
+    return redirect(f'https://wa.me/{whatsapp}?text=Saya tertarik dengan {nama} seharga {harga}. Tolong informasi lebih lanjut.')
+
 
 @app.route('/gallery')
 def gallery():
     return render_template('gallery.html')
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/upmerch')
-def upmerch():
-    return render_template('upmerch.html')
-
-@app.route('/add', methods=['POST'])
-def add_merchandise():
-    if 'image' not in request.files:
-        return 'No file part'
-    file = request.files['image']
-    if file.filename == '':
-        return 'No selected file'
-    if file:
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        description = request.form['description']
-        price = float(request.form['price'])
-        merchandise = {
-            'image': filename,
-            'description': description,
-            'price': price
-        }
-        collection.insert_one(merchandise)
-        return redirect(url_for('index'))
-
-@app.route('/delete/<id>')
-def delete_merchandise(id):
-    merchandise = collection.find_one({'_id': ObjectId(id)})
-    if merchandise:
-        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], merchandise['image']))
-        collection.delete_one({'_id': ObjectId(id)})
-    return redirect(url_for('index'))
-
-@app.route('/edit/<id>', methods=['GET', 'POST'])
-def edit_merchandise(id):
-    merchandise = collection.find_one({'_id': ObjectId(id)})
+@app.route('/upmerchandise', methods=['GET', 'POST'])
+def upmerchandise():
     if request.method == 'POST':
-        if 'image' in request.files:
-            file = request.files['image']
-            if file.filename != '':
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                merchandise['image'] = filename
-        merchandise['description'] = request.form['description']
-        merchandise['price'] = float(request.form['price'])
-        collection.update_one({'_id': ObjectId(id)}, {'$set': merchandise})
-        return redirect(url_for('index'))
-    return render_template('edit.html', merchandise=merchandise)
+        nama = request.form['nama']
+        harga = request.form['harga']
+        deskripsi = request.form['deskripsi']
+        nama_gambar = request.files['gambar']
+
+        if nama_gambar:
+            nama_file_asli = nama_gambar.filename
+            nama_file_gambar = nama_file_asli.split('/')[-1]
+            file_path = f'static/assets/img/product/{nama_file_gambar}'
+            nama_gambar.save(file_path)
+        else:
+            nama_file_gambar = None
+
+        doc = {
+            'nama': nama,
+            'harga': harga,
+            'gambar': nama_file_gambar,
+            'deskripsi': deskripsi
+        }
+        db.merchandise.insert_one(doc)
+        return redirect(url_for('upmerchandise'))
+
+    merchandise = list(db.merchandise.find({}))
+    return render_template('up-merchandise.html', merchandise=merchandise)
+
+@app.route('/editMerchandise/<_id>', methods=['GET', 'POST'])
+def editMerchandise(_id):
+    if request.method == 'POST':
+        id = request.form['_id']
+        nama = request.form['nama']
+        harga = request.form['harga']
+        deskripsi = request.form['deskripsi']
+        nama_gambar = request.files['gambar']
+        
+        doc = {
+            'nama': nama,
+            'harga': harga,
+            'deskripsi': deskripsi
+        }
+        if nama_gambar:
+            nama_file_asli = nama_gambar.filename
+            nama_file_gambar = nama_file_asli.split('/')[-1]
+            file_path = f'static/assets/img/product/{nama_file_gambar}'
+            nama_gambar.save(file_path)
+            doc['gambar'] = nama_file_gambar
+        
+        db.merchandise.update_one({"_id": ObjectId(_id)}, {"$set": doc})
+        return redirect(url_for("upmerchandise"))
+    
+    id = ObjectId(_id)
+    merchandise = db.merchandise.find_one({"_id": id})
+    return jsonify({'nama': merchandise['nama'], 'harga': merchandise['harga'], 'deskripsi': merchandise['deskripsi'], 'gambar': merchandise['gambar']})
+
+@app.route('/deleteMerchandise/<_id>',methods=['GET','POST'])
+def deleteMerchandise(_id):
+   id = ObjectId(_id)
+   db.merchandise.delete_one({"_id": ObjectId(_id)})
+   return redirect(url_for("upmerchandise"))
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
